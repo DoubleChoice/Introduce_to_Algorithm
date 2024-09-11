46023d329fa32c804033b4e84e6d6b58760a38e6
DoubleChoice <53597882+DoubleChoice@users.noreply.github.com>
2024-09-11 23:02:19 +0800

    Create insert_sort.cpp

---
+++ insert_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> arr({ 5,2,4,6,1,3 });
+
+void insert_sort(vector<int>& arr) {
+	for (int j = 1; j < arr.size(); j++) {
+		int i = j - 1, temp = arr[j];
+		while (i>=0 && arr[i]>temp) {
+			arr[i + 1] = arr[i];
+			i--;
+		}
+		arr[i + 1] = temp;
+	}
+}
+
+int main()
+{
+	insert_sort(arr);
+	for (int i = 0; i < arr.size(); i++) {
+		cout << arr[i] <<endl;
+	}
+}
